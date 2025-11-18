1a3c5b7ee485faf52728ba1811df774ebc498610
HebaHamdan2 <hebashamdan2@gmail.com>
2025-11-18 21:11:44 +0200

    detect offline navigator.onLine

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import { useAppSelector } from "@/app/hooks";
+import OfflinePage from "@/errors/OfflinePage";
 
 const RootLayout = () => {
   const { userType } = useAppSelector((state) => state.auth);
+  const [online, setOnline] = useState(true);
+
+  useEffect(() => {
+    const handleOnline = () => setOnline(true);
+    const handleOffline = () => setOnline(false);
+
+    setTimeout(() => setOnline(navigator.onLine), 100);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  if (!online) return <OfflinePage />;
 
   return (
     <>
